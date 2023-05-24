ecc316f48e25a2681ecaf50902099fc469381ed2
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-05-24 15:07:43 -0400

    Función mostrar imágenes finalizada

--- src/pages/Imagenes.vue
+++ src/pages/Imagenes.vue
@@ -1,18 +1,42 @@
 <template>
   <div>
-    <h1>Imágenes</h1>
-    <div>
-
+    <h1>Imágenes de {{ razaPerro }}</h1>
+    <div class="contenedor">
+        <div v-for="imagen in imagenes" :key="imagen">
+            <img :src="imagen" alt="Imagen de Perro">
+        </div>
     </div>
   </div>
 </template>
 
 <script>
+import { obtenerImagenes } from '../services/dog_api.services';
 export default {
     name: "ImagenesPage",
+    data(){
+        return {
+            datos: [],
+            imagenes: [],
+            razaPerro: "",
+        }
+    },
+    async mounted(){
+        this.razaPerro = this.$route.params.raza;
+      const result = await obtenerImagenes(this.razaPerro);
+        this.datos = result;
+        this.imagenes = this.datos.message
+        console.log(this.imagenes)
+    },
 }
 </script>
 
 <style>
-
+    .contenedor {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    img {
+        height: 500px;
+        width: 500px;
+    }
 </style>
